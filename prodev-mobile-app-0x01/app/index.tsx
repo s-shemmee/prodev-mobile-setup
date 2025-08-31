68ef2899e54b146001a0cc7bd6a436b474cd3210
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-31 12:21:34 +0100

    refactor: rename App component to Home and clean up imports

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,25 +1,22 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-<View>
-<Text style={styles.largeText}>Typescript is great if you practice more</Text>
-<Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-<Text style={styles.smallText}>ALX is awesome</Text>
-</View>
-</View>
+      
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   largeText: {
     fontSize: 30,
