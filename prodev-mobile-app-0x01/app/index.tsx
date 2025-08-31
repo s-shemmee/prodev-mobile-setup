be2420fac1a38f6ccd537abe9defceafbf7a0910
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-31 12:27:39 +0100

    fix: adjust indentation in App component for improved readability

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -5,12 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-  <View>
-  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-  <Text style={styles.smallText}>ALX is awesome</Text>
-</View>
-</View>
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
+    </View>
   );
 }
 
